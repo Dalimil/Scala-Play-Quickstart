f2c486a7331663413616bb620032e150917c0d8d
Dalimil <dalimilhajek@gmail.com>
2016-04-07 13:56:55 +0100

    Add basic GET request-response examples

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Scala-Play-Quickstart
 Basic Scala web application using the Play Framework
+
+### TODO
+- [ ] JSON
+- [ ] Server-side requests and API connections
+- [ ] OAuth
+- [ ] Database and ORM
+- [ ] WebSockets example with channels/rooms
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,38 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 class Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready"))
+  def index = Action { implicit request => // Make implicit to be able to use it later
+    val myCookie = request.cookies.get("theme") // Option[Cookie]
+    val mySession = request.session.get("connected") // Option[String]
+
+    Ok(views.html.index("Your new application is ready")) // Send an html template
+      .withHeaders(CACHE_CONTROL -> "max-age=3600") // Add header
+      .withCookies(Cookie("theme", "blue")) // Add cookie
+      .discardingCookies(DiscardingCookie("skin")) // Remove cookie
+      .withSession(request.session + ("connected" -> "user@gmail.com")) // Add to session (can only be String)
+      .withSession(request.session - "theme") // Remove from session
+  }
+
+  def debug = Action { request =>
+    Ok("Got request: "+ request)
+  }
+
+  def test(id: Long) = TODO
+
+  def flashDemo = Action { implicit request =>
+    val fl = request.flash.get("success")
+    if(fl.isDefined){
+      Ok(fl.get)
+    } else {
+      Redirect("/flash").flashing("success" -> "You have been logged in")
+    }
+  }
+
+  def showUser(name: String) = Action {
+    Ok("Hi: "+name)
   }
 
 }
