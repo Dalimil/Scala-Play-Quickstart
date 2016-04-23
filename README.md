c5fa69a610e746155ca9fd056e0e38f1a9badacb
Dalimil <dalimilhajek@gmail.com>
2016-04-24 00:06:33 +0100

    Finish WebSocket demo

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ Basic Scala web application using the Play Framework
 https://playframework.com/documentation/2.5.x/Home
 
 ### TODO
-- [ ] WebSockets example with channels/rooms
 - [ ] Find a JSON package that can convert Map[String, Any] <-> String (Json format)
     - [ ] GSON in Scala?
--- app/controllers/WebSocketController.scala
+++ app/controllers/WebSocketController.scala
@@ -10,25 +10,48 @@ import scala.concurrent.ExecutionContext
 class WebSocketController @Inject() (implicit ec: ExecutionContext) extends Controller {
 
   def index = Action {
-    Ok(views.html.sockets_example())
+    Ok(views.html.sockets_example()).withSession(("username", java.util.UUID.randomUUID().toString))
   }
 
-  // Concurrent.broadcast returns (Enumerator, Concurrent.Channel)
-  val (out, channel) = Concurrent.broadcast[String]
+  def debug = Action {
+    pushAll
+    Ok("done")
+  }
+
+  /** global channel for everyone - not used at the moment */
+  val (broadcast, globalChannel) = Concurrent.broadcast[String] // returns (Enumerator, Concurrent.Channel)
+
+  /** every user has his own channel */
+  var users = Map[String, (Enumerator[String], Concurrent.Channel[String])]()
+
+  /** users can be grouped into rooms - todo */
+  var groups = Map[String, Set[String]]()
+
+  def pushAll = users.foreach(x => x._2._2.push("Hello - " + x._1)) // globalChannel.push("Hello everyone")
+
+  def pushOne(username: String) = users(username)._2.push("Hello "+ username)
 
   /** GET endpoint (is automatically upgraded to WS) - can have multiple of these */
-  def ws = WebSocket.using[String] { request =>
-    // Enumerator and channel could also be created here (different for each user)
+  def ws = WebSocket.using[String] { implicit request =>
+    val username = request.session.get("username").get // or uuid previously stored in session
 
-    // log the received message to stdout and send response back to client
-    val in = Iteratee.foreach[String] {
-      msg => Logger.info(msg)
-        // the Enumerator returned by Concurrent.broadcast subscribes to the channel and will
-        // receive the pushed messages
+    if(users.contains(username)) { // existing user
+      (Iteratee.ignore[String], users(username)._1) // (in, out)
+    } else {
+      // create a new private channel
+      val (enumerator, channel) = Concurrent.broadcast[String] // only broadcasts for people using the same channel
+      users += ((username, (enumerator, channel)))
+
+      val in = Iteratee.foreach[String]( msg => { // do something with an incoming message
+        Logger.info(msg)
         channel.push("I received your message: " + msg)
-    }
+      }).map{ _ => // WebSocket closed by the user
+        users(username)._2.eofAndEnd() // close channel
+        users -= username
+      }
 
-    (in,out) // always return both like this
+      (in, enumerator) // always return both like this
+    }
   }
 
 }
